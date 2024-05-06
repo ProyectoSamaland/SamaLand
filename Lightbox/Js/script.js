d207ba984a7436d59f12bfdfe9331487ddbbfc93
SamaLandInicioRoger <rogermirandaair@gmail.com>
2024-05-06 18:58:46 -0500

    Actualización de contenido e imágenes

--- Lightbox/Js/script.js
+++ Lightbox/Js/script.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.querySelector("#contact form");
     contactForm.addEventListener("submit", function(event) {
         event.preventDefault(); // Previene el envío del formulario
-        alert("Gracias por contactarnos. Te responderemos pronto.");
+        alert("Gracias por contactarnos. Te responderemos cuanto antes.");
     });
 });
